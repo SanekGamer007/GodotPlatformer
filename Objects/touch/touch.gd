b9ee4ae7af130a79b2c9028374bb4a70744d429d
SanekGamer007 <100200807+SanekGamer007@users.noreply.github.com>
2025-03-28 15:50:45 +0100

    fix most if not all jitter issues

--- Objects/touch/touch.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _process(_delta: float) -> void:
-	if !%player.get_node("PlayerCam"):
-		push_error("Cannot find node \"PlayerCam\", touch controls wont work!")
-		set_process(false)
-		set_visible(false)
-	else:
-		set_position(%player.get_node("PlayerCam").get_screen_center_position() - get_viewport_rect().size / 2)
