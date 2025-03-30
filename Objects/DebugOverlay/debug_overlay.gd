73aac77d318d421093354b37084ae46e3097e3df
SanekGamer007 <100200807+SanekGamer007@users.noreply.github.com>
2025-03-30 16:14:14 +0200

    improve the death effect

--- Objects/DebugOverlay/debug_overlay.gd
+++ Objects/DebugOverlay/debug_overlay.gd
@@ -3,4 +3,4 @@ extends Node
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	get_node("FPS").text = "FPS: " + "%.2f" % (1.0/delta)
-	get_node("TimeSinceLastFrame").text = "TSLF: " + "%.4f" % delta + "ms"
+	get_node("FrameTime").text = "Frametime: " + "%.4f" % delta + "ms"
