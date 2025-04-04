8d5973878590f424a5f910b75b7f22ac09a1e843
SanekGamer007 <100200807+SanekGamer007@users.noreply.github.com>
2025-04-04 12:38:47 +0200

    finish player rewrite, kinda fix the code style

--- Maps/test_map/test_map.gd
+++ Maps/test_map/test_map.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
 func _process(_delta):
-	if %player.global_position.y > 640 and %player.isalive == true:
-		%player.kill()
+	if %player.global_position.y > 640 and %player.state != %player.states.DEAD:
+		%player.set_state(%player.states.DEAD)
--- Objects/playernew/player_new.gd
+++ Objects/playernew/player_new.gd
@@ -1,5 +1,12 @@
 extends CharacterBody2D
-enum states {IDLE, MOVING, JUMPING, FALLING, DEAD, DASHING}
+enum states {
+	IDLE, 
+	MOVING, 
+	JUMPING,
+	FALLING, 
+	DEAD, 
+	DASHING,
+	}
 var statename = {
 	0: "IDLE",
 	1: "MOVING",
@@ -14,27 +21,25 @@ var state: states = states.IDLE
 
 @export_group("Movement","mv_")
 @export var mv_MAX_SPEED: int = 130 ## Max Horizontal player speed.
+@export var mv_MAX_DASH_SPEED: int = 150 + mv_MAX_SPEED ## WIP
 @export var mv_MAX_VERTICAL_SPEED: int = -240 ## Max Vertical player speed.
 @export var mv_NV_MAX_VERTICAL_SPEED: int = 360 ## Max Negative Vertical player speed.
 @export var mv_INITIAL_JUMP_HEIGHT: int = 120 ## Lowest Jump force.
 @export var mv_ADDITIONAL_JUMP_HEIGHT: int = 500 ## Highest Jump force.
 @export var mv_CELERATION_STEPS: int = 12 ## How much steps does it require for the player to come to stop / to full speed.
-@export var mv_DASHSPEED: Vector2 = Vector2(160, 220) ## WIP
+@export var mv_DASHSPEED: Vector2 = Vector2(140, 200) ## WIP
 @export var mv_SPEED_REDUCTION_RATE: int = 16 ## WIP
 @export var mv_SPEED_REDUCTION_RATE_AIR: int = 4 ## WIP
+@export var isgod: bool = false
 var mv_ACCELERATION: int = mv_MAX_SPEED / mv_CELERATION_STEPS ## Acceleration.
 var mv_DECELERATION: int = mv_MAX_SPEED / mv_CELERATION_STEPS ## Deceleration.
-var candash: bool = true
-#var isalive: bool = true
-#var can_dash: bool = true
-#var was_on_floor: bool
-#var can_jump: bool ## Defines if the player can jump.
+var candash: bool = true ## WIP
 
 
 func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		candash = true
-	print(statename.get(state), " ", velocity, " ", is_on_floor())
+	#print(statename.get(state), " ", velocity, " ", is_on_floor())
 	if Input.is_action_just_pressed("dash"):
 		set_state(states.DASHING)
 	elif velocity.y > 1:
@@ -51,7 +56,9 @@ func _physics_process(delta: float) -> void:
 		if not abs(velocity.x) > mv_MAX_SPEED: 
 			velocity.x = velocity.x + mv_ACCELERATION * direction # if below max speed add more velocity
 		elif is_on_floor():
-			velocity.x = velocity.x + ((sign(velocity.x) * mv_SPEED_REDUCTION_RATE) * -1) #if above max speed start decreasing velocity
+			velocity.x = (
+				#if above max speed start decreasing velocity
+				velocity.x + ((sign(velocity.x) * mv_SPEED_REDUCTION_RATE) * -1))
 		else:
 			velocity.x = velocity.x + ((sign(velocity.x) * mv_SPEED_REDUCTION_RATE_AIR) * -1) #if above max speed start decreasing velocity
 	else:
@@ -65,12 +72,19 @@ func _physics_process(delta: float) -> void:
 		velocity.y = -mv_INITIAL_JUMP_HEIGHT
 		#print("jumping")
 	if velocity.y < 0:
-		if Input.is_action_pressed("ui_accept") and not is_on_floor(): # i have no idea why the fuck it works but it does (fixes a bug when you can do the additional jump force in the air after walking off a platform without jumping)
+		# i have no idea why the fuck it works but it does
+		# (fixes a bug when you can do the additional jump
+		# force in the air after walking off a platform without jumping)
+		if Input.is_action_pressed("ui_accept") and not is_on_floor():
 			velocity.y -= mv_ADDITIONAL_JUMP_HEIGHT * delta
 			#print("additional jump active")
 	if state == states.DASHING:
 		var dashdirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-		velocity = velocity + (mv_DASHSPEED * dashdirection)
+		print(abs(velocity + (mv_DASHSPEED * dashdirection)))
+		if abs(velocity + (mv_DASHSPEED * dashdirection)) > Vector2(mv_MAX_DASH_SPEED, mv_MAX_DASH_SPEED):
+			velocity = velocity + ((mv_DASHSPEED * dashdirection) / 2.2)
+		else:
+			velocity = velocity + (mv_DASHSPEED * dashdirection)
 		print(dashdirection)
 
 	
@@ -82,8 +96,8 @@ func _physics_process(delta: float) -> void:
 
 func set_state(new_state: int) -> void:
 	var previous_state := state
-	print(statename.get(previous_state), " ", statename.get(new_state))
-#	if previous_state == states.MOVING and new_state == states.DASHING or previous_state == states.FALLING and new_state == states.DASHING:
+	#print(statename.get(previous_state), " ", statename.get(new_state))
+	#if previous_state == states.MOVING and new_state == states.DASHING or previous_state == states.FALLING and new_state == states.DASHING:
 	if new_state == states.DASHING:
 		#if is_on_floor():
 			#if get_node("DashTimer").is_stopped() == true:
@@ -135,4 +149,5 @@ func reset():
 
 
 func _on_hurtbox_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	set_state(states.DEAD)
+	if not isgod:
+		set_state(states.DEAD)
